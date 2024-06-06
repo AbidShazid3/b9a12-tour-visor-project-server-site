5057611c31787533a21f05c5d7e8069c389f4cce
AbidShazid3 <abidshazid3@gmail.com>
2024-06-06 18:39:56 +0600

    added all stories single data get

--- index.js
+++ index.js
@@ -93,6 +93,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/stories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await storiesCollection.findOne(query);
+            res.send(result);
+        })
+
         // package related
         app.get('/packages', async (req, res) => {
             const result = await packagesCollection.find().toArray();
