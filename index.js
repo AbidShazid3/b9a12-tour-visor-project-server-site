fb4351e5d7b4b0081bdc3c82a67377ababa4ec8f
AbidShazid3 <abidshazid3@gmail.com>
2024-06-07 18:44:50 +0600

    added all users data get

--- index.js
+++ index.js
@@ -58,6 +58,11 @@ async function run() {
         }
 
         // user related
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
         app.put('/user', async (req, res) => {
             const user = req.body;
             const query = { email: user?.email }
